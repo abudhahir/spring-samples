cdb9f32cdf2f29583f2dee5193972dc25c591324
TOLABAB2 <abdul.abudhahir@swisscom.com>
2021-09-12 18:07:43 +0200

    mismatch

---
+++ message-ssimulator/src/main/java/com/cleveloper/messagessimulator/MessageSsimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.cleveloper.messagessimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSsimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSsimulatorApplication.class, args);
+    }
+
+}
---
+++ message-ssimulator/src/test/java/com/cleveloper/messagessimulator/MessageSsimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleveloper.messagessimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSsimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
